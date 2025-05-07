eddcbd3bad58b9796c1966ddbcb998e997459ab1
Ana Carolina Manzan <ana.manzan@gmail.com>
2025-05-07 17:53:20 -0300

    Initial commit

---
+++ iac/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "novo-app-uni"
+  location = "eastus2"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "uniteste-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"
+  sku_name            = "S1"
+}
+
+resource "azurerm_windows_web_app" "webapp_unianchieta" {
+  depends_on = [
+    azurerm_service_plan.asp
+  ]
+
+  name                = "mznhiehrwo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack { 
+      dotnet_version = "v8.0"
+      current_stack  = "dotnet"
+    }
+  }
+}
---
+++ iac/version.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "teste-tf"
+    storage_account_name = "stgtfmzn01devdemo"
+    container_name       = "tfstate"
+    key                  = "tf.tfstate"
+    use_oidc = true
+  }
+}
+
+provider "azurerm" {
+  use_oidc = true
+  features {}
+}
+
+provider "azuread" {
+}
+
+data "azurerm_client_config" "current" {}
